f7cb00ee454cd4a8ef1c857f07179e0138002ae8
chathuri <chathuriimasha60@gmail.com>
2022-07-11 22:01:20 -0700

    Add id

--- src/Controller/studentFormController.java
+++ src/Controller/studentFormController.java
@@ -1,11 +1,29 @@
 package Controller;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
 
 public class studentFormController {
+    public JFXTextField txtId;
+    public JFXTextField txtName;
+    public JFXTextField txtMail;
+    public JFXTextField txtContact;
+    public JFXTextField txtAddress;
+    public JFXTextField txtNic;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colMail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+
     public void btnRemove(ActionEvent actionEvent) {
     }
 
     public void btnUpdate(ActionEvent actionEvent) {
+    }
+
+    public void btnSave(ActionEvent actionEvent) {
     }
 }
---
+++ src/db/dbConnection.java
@@ -0,0 +1,25 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+    private static dbConnection dbConnection = null;
+    private Connection connection;
+
+
+    private dbConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/manageStudent ", "root", "1234");
+    }
+
+
+    public static dbConnection getInstance() throws SQLException, ClassNotFoundException {
+        return(dbConnection==null)? dbConnection= new dbConnection(): dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection; // DBConnection.getInstance().getConnection();
+    }
+}
