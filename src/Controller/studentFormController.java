cb38e6607c2e379bb526b84f260067c0f9d0bd26
chathuri <chathuriimasha60@gmail.com>
2022-07-11 21:30:56 -0700

    create UI

--- src/Controller/studentFormController.java
+++ src/Controller/studentFormController.java
@@ -1,4 +1,11 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+
 public class studentFormController {
+    public void btnRemove(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdate(ActionEvent actionEvent) {
+    }
 }
